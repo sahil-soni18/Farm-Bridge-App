f4c1a28794c05f4e737638ce634b911a3fa26a79
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-01-19 23:36:49 +0530

    solve error in images loading and button color

--- Farm-Bridge-Frontend/app/(tabs)/index.tsx
+++ Farm-Bridge-Frontend/app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ import ExploreProductsScreen from '../Screens/ExploreProductsScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const AppNavigator = () => (
-  <Stack.Navigator initialRouteName="Login">
+  <Stack.Navigator initialRouteName="ExploreProducts">
     <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
     <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
     <Stack.Screen name="Admin" component={AdminScreen} options={{ headerShown: false }} />
